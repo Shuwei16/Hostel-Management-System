8361d06d2e016649f9ce93347b50bb3b0ebe8ef9
Shuwei16 <wangsw-wm20@student.tarc.edu.my>
2023-12-27 12:20:41 +0800

    final final final

--- database/seeders/VisitorSeeder.php
+++ database/seeders/VisitorSeeder.php
@@ -15,21 +15,18 @@ class VisitorSeeder extends Seeder
      */
     public function run(): void
     {
-        //dummy data for visitor registration record
-        for($i=1; $i <= 15; $i++) {
-
-            VisitorRegistration::create([
-                'student_id' => rand(1, 5),
-                'visitor_name' => 'Lee Ah Meng',
-                'visit_purpose' => 'Drop luggages',
-                'visit_date' => '2023-11-23',
-                'visit_time' => "9:00:00",
-                'duration' => "10",
-                'status' => 'Pending Approval',
-            ]);
-        }
 
         //dummy data for the first approved visitor
+        VisitorRegistration::create([
+            'student_id' => 1,
+            'visitor_name' => 'Lee Ah Meng',
+            'visit_purpose' => 'Drop luggages',
+            'visit_date' => '2023-11-23',
+            'visit_time' => "9:00:00",
+            'duration' => "10",
+            'status' => 'Pending Approval',
+        ]);
+
         // Generate a QR code
         $qrCode = QrCode::format('svg')->size(200)->generate('admin-visitorRegistrationDetails-1');
 
@@ -41,5 +38,20 @@ class VisitorSeeder extends Seeder
         // Save QR code in db
         VisitorRegistration::find(1)
                            ->update(['status' => 'Approved', 'qr_code' => $fileName]);
+
+
+        //dummy data for visitor registration record
+        for($i=2; $i <= 15; $i++) {
+
+            VisitorRegistration::create([
+                'student_id' => rand(1, 5),
+                'visitor_name' => 'Lee Ah Meng',
+                'visit_purpose' => 'Drop luggages',
+                'visit_date' => '2023-11-23',
+                'visit_time' => "9:00:00",
+                'duration' => "10",
+                'status' => 'Pending Approval',
+            ]);
+        }
     }
 }
